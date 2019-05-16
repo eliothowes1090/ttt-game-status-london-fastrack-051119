f31e0eb5e8b70660de6b8d957b2660851e7a21ea
Eliot Howes <ejhowes@gmail.com>
2019-05-16 21:20:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,11 +51,11 @@ def winner(board)
   # position_3 = won?(board)[2]
     # if over?(board) && !won?(board)
     #   return nil
-    if board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "X"
+  if draw?(board)
+      return nil
+  elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "X"
       return "X"
     elsif board[won?(board)[0]] && board[won?(board)[1]] && board[won?(board)[2]] == "O"
       return "O"
-    elsif draw?(board)
-      return nil
     end
 end
